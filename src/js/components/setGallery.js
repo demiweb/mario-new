7adee88a32b71eee1e948beec13d5c24f931c2da
yurayarosh <yura12323@gmail.com>
2019-12-18 16:02:29 +0200

    change item-presentation

--- src/js/components/setGallery.js
+++ src/js/components/setGallery.js
@@ -1,12 +1,48 @@
 import 'lightgallery.js'
 
 export default function setGallery() {
-  const lgs = [...document.querySelectorAll('.js-lightgallery')]
+  function initGalleries() {
+    const lgs = [...document.querySelectorAll('.js-lightgallery')]
 
-  if (!lgs.length) return
+    if (!lgs.length) return
 
-  lgs.forEach(lg => {
-    // eslint-disable-next-line
-    lightGallery(lg);
-  })
+    lgs.forEach(lg => {
+      // eslint-disable-next-line
+      lightGallery(lg)
+    })
+  }
+
+  function initTriggerGallery() {
+    const btnClass = 'js-lightgallery-trigger'
+
+    const onClick = e => {
+      const btn = e.target.closest(`.${btnClass}`)
+      if (!btn) return
+      e.preventDefault()
+
+      const imgs = btn.dataset.images
+      if (!imgs) return
+
+      const dynamicEl = []
+
+      const images = imgs.split(',')
+      images.forEach(src => {
+        const obj = {
+          src: src.trim(),
+        }
+        dynamicEl.push(obj)
+      })
+
+      // eslint-disable-next-line
+      lightGallery(btn, {
+        dynamic: true,
+        dynamicEl,
+      })
+    }
+
+    document.addEventListener('click', onClick)
+  }
+
+  initGalleries()
+  initTriggerGallery()
 }
